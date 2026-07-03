ee9a4466a457b53722f3ffed9f166e476e1768dd
roberiobessa-cloud <roberio.bessa@clinicasim.com>
2026-07-03 14:01:09 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey:            "AIzaSyAfz-evo3jddKNwVEqdrL2yBAn-FhuRxQQ",
+  authDomain:        "peo-clinica-sim.firebaseapp.com",
+  projectId:         "peo-clinica-sim",
+  storageBucket:     "peo-clinica-sim.firebasestorage.app",
+  messagingSenderId: "474441198245",
+  appId:             "1:474441198245:web:b96701264bdf77a5877b4c"
+};
